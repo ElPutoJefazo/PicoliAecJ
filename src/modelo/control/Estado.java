b65b8e94ffb3b0d70fb92de00a32f4b2ad66f4da
joaquindag <49230060+joaquindag@users.noreply.github.com>
2019-05-11 13:35:41 +0200

    Organizacion de todo

---
+++ src/modelo/control/Estado.java
@@ -0,0 +1,53 @@
+package modelo.control;
+
+import java.util.ArrayList;
+
+public class Estado {
+
+	ArrayList<Seres> listaMenores = new ArrayList<Seres>();
+	ArrayList<Seres> listaAdultos = new ArrayList<Seres>();
+	ArrayList<Seres> listaJubilados= new ArrayList<Seres>();
+	
+	ArrayList<Empresa> empresas = new ArrayList<Empresa>();
+	
+	private long dineroEstado=100000;
+	private int nacimientos=0;
+	private int jubilados=0;
+	
+	
+	private void nacer() {
+		
+	}
+	
+	private void morir() {
+		
+	}
+	
+	private void aumentarAnno() {
+		
+	}
+	
+	private void incrementarProduccion() {
+		
+	}
+	
+	private void decrementarProduccion() {
+		
+	}
+	
+	private void subsidio() {
+		
+	}
+	
+	private void pagarTrabajador() {
+		
+	}
+	
+	private void annadirEmpresa() {
+		
+	}
+	
+	private void eliminarEmpresa() {
+		
+	}
+}
